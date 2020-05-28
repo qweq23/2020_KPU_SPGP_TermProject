cfc1eb07f7600c9c3800196d896ccb968d3d0a6c
qweq23 <qweq23@naver.com>
2020-05-28 17:48:44 +0900

    View 기획
    어떤 뷰가 필요하고 서로 어떤 전환 구조를 갖는 지 기획한 것을
    메인 스토리보드에 표현함

---
+++ BookManager/BookManager/BookDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BookDetailViewController.swift
+//  BookManager
+//
+//  Created by kpugame on 2020/05/28.
+//  Copyright © 2020 kpugame. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    
+    @IBAction func saveToBookDetail(segue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func cancelToBookDetail(segue: UIStoryboardSegue) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookManager/BookManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookManager
+//
+//  Created by kpugame on 2020/05/26.
+//  Copyright © 2020 kpugame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
